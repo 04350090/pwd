053d4987eb122dffc9059cc866e105b4071122d8
Alan Tai <alan5710804@gmail.com>
2020-10-20 23:05:52 +0800

    Final Version

--- pwd.py
+++ pwd.py
@@ -1,11 +1,15 @@
 password = 'a123456'
 n = 3
 while n > 0:
+	n = n - 1
 	login = input('請輸入密碼：')
 	if login == password:
 		print('登入成功')
 		break
 	else:
-		n = n-1
-		print('密碼錯誤！還有',n,'次機會')
+		print('密碼錯誤！')
+		if n > 0:
+			print('還有',n,'次機會')
+		else:
+			print('沒機會嘗試了')
 
